9b2b14d9477cb07d49c1695a96ad236e4e18562b
zqsnnn <95234019+zqsnnn@users.noreply.github.com>
2023-02-27 22:14:04 +0800

    Master (#2) (#4)
    * init
    
    * init framework
    
    ---------
    
    Co-authored-by: 胡海乐 <19214109080@qq.com>

---
+++ src/main/java/com/example/sedemo/utils/result/Result.java
@@ -0,0 +1,11 @@
+package com.example.sedemo.utils.result;
+
+public class Result<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    public Result(){
+
+    }
+}
