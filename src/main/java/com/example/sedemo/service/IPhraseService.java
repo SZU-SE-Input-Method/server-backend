112edbfe8e91629f18d7cd26967bc49c3162326a
Liu Ye <1316519465@qq.com>
2023-03-01 21:22:45 +0800

    初步完成私人短语模块接口 【后台短语模块】 https://www.tapd.cn/59969215/prong/stories/view/1159969215001002660

--- src/main/java/com/example/sedemo/service/IPhraseService.java
+++ src/main/java/com/example/sedemo/service/IPhraseService.java
@@ -1,7 +1,10 @@
 package com.example.sedemo.service;
 
-import com.example.sedemo.entity.Phrase;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.sedemo.entity.Phrase;
+import com.example.sedemo.result.Result;
+
+import java.time.LocalDate;
 
 /**
  * <p>
@@ -12,5 +15,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-02-28
  */
 public interface IPhraseService extends IService<Phrase> {
+
+    Result getPhraseById(Long pid);
+
+    Result phrasePage(Integer pageNum, Integer pageSize, String content, Long uid, LocalDate createTime);
 
 }
--- src/main/java/com/example/sedemo/service/impl/PhraseServiceImpl.java
+++ src/main/java/com/example/sedemo/service/impl/PhraseServiceImpl.java
@@ -1,10 +1,16 @@
 package com.example.sedemo.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.sedemo.entity.Phrase;
 import com.example.sedemo.mapper.PhraseMapper;
+import com.example.sedemo.result.Result;
 import com.example.sedemo.service.IPhraseService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
 
 /**
  * <p>
@@ -16,5 +22,25 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PhraseServiceImpl extends ServiceImpl<PhraseMapper, Phrase> implements IPhraseService {
+    @Override
+    public Result getPhraseById(Long pid) {
+        Phrase phrase = this.getById(pid);
+        if (phrase == null) {
+            return Result.error().msg("未查询到对应私人短语");
+        }
+        return Result.success().data(phrase).msg("查询具体私人短语成功");
+    }
 
+    @Override
+    public Result phrasePage(Integer pageNum, Integer pageSize, String content, Long uid, LocalDate createTime) {
+        Page<Phrase> phrasePage = new Page<>(pageNum, pageSize);
+        //构造查询条件
+        LambdaQueryWrapper<Phrase> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(StringUtils.hasText(content), Phrase::getContent, content);
+        queryWrapper.like(createTime != null, Phrase::getCreateTime, createTime);
+        queryWrapper.eq(uid != null, Phrase::getUid, uid);
+        //调用mp接口
+        this.page(phrasePage, queryWrapper);
+        return Result.success().data(phrasePage).msg("分页查询私人短语成功");
+    }
 }
