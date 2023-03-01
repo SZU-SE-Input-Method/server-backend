c93044e9ee5a568cd1039b05714dee4fd8a3e6b5
Liu Ye <1316519465@qq.com>
2023-03-01 21:21:39 +0800

    初步完成用户模块接口 【用户管理模块】 https://www.tapd.cn/59969215/prong/stories/view/1159969215001002717

--- src/main/java/com/example/sedemo/controller/UserController.java
+++ src/main/java/com/example/sedemo/controller/UserController.java
@@ -1,19 +1,20 @@
 package com.example.sedemo.controller;
 
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.sedemo.Dto.UserDto;
 import com.example.sedemo.entity.User;
 import com.example.sedemo.result.Result;
 import com.example.sedemo.service.IUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author LiuYe
@@ -21,6 +22,7 @@ import java.util.Date;
  */
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
     @Autowired
     private IUserService userService;
@@ -29,24 +31,33 @@ public class UserController {
     }
 
     @PostMapping
-    public Result<String> saveUser(@Validated @RequestBody User user) {
-        userService.saveUser(user);
-
+    public Result saveUser(@Validated @RequestBody UserDto userDTO) {
+        log.info("调用查询用户接口,接受数据:{}", userDTO);
+        userService.saveUser(User.dtoToUser(userDTO));
         return Result.success().msg("用户新增成功");
     }
 
     @PutMapping
-    public Result<String> updateUser(@Validated @RequestBody User user) {
-        if (user.getUid() == null) {
+    public Result updateUser(@Validated @RequestBody UserDto userDTO) {
+        if (userDTO.getUid() == null) {
             return Result.error().msg("接口调用报错:用户id不可为空");
         }
-        userService.updateUser(user);
-
+        log.info("调用更新用户接口,接受数据:{}", userDTO);
+        userService.updateUser(User.dtoToUser(userDTO));
         return Result.success().msg("用户信息修改成功");
     }
 
+    @GetMapping("/{uid}")
+    public Result getById(@PathVariable Long uid) {
+        if (uid == null) {
+            return Result.error().msg("查询失败,请使用用户id查询");
+        }
+        log.info("调用查询用户接口,接受数据:uid = {}", uid);
+        return userService.getUserById(uid);
+    }
+
     @GetMapping("/page/{pageNum}/{pageSize}")
-    public Result<Page<User>> page(
+    public Result page(
             @PathVariable Integer pageNum,
             @PathVariable Integer pageSize,
             String name,
@@ -54,8 +65,11 @@ public class UserController {
             Boolean gender,
             String phone,
             String email,
-            Date createTime) {
-        Page<User> page = userService.userPage(pageNum, pageSize, name, username, gender, phone, email, createTime);
-        return Result.success().data(page);
+            LocalDateTime createTime) {
+        if (pageNum == null || pageSize == null) {
+            return Result.error().msg("当前页码或每页数量参数缺失");
+        }
+        log.info("调用分页查询用户列表接口");
+        return userService.userPage(pageNum, pageSize, name, username, gender, phone, email, createTime);
     }
 }
