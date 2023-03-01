9d02502b5e945d89730fbb0c2d0600eab93e7358
Liu Ye <1316519465@qq.com>
2023-03-01 21:25:16 +0800

    通用组件更新

--- src/main/java/com/example/sedemo/controller/AdminController.java
+++ src/main/java/com/example/sedemo/controller/AdminController.java
@@ -4,6 +4,7 @@ package com.example.sedemo.controller;
 import com.example.sedemo.entity.Admin;
 import com.example.sedemo.result.Result;
 import com.example.sedemo.service.IAdminService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author LiuYe
  * @since 2023-02-28
  */
+@Slf4j
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
@@ -34,6 +36,7 @@ public class AdminController {
     */
     @PostMapping("/login")
     public Result authAdministrator(@Validated @RequestBody Admin admin) {
+        log.info("调用管理员认证接口");
         if(adminService.auth(admin)) {
             return Result.success().msg("管理员登录成功");
         }
