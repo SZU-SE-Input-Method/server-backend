e7ad7c9bdc36cd3008d3994ed711f07718f7d800
Liu Ye <1316519465@qq.com>
2023-03-02 11:11:09 +0800

    update 2023年3月2日11:10:51

--- deployment/server_backend_update.sh
+++ deployment/server_backend_update.sh
@@ -4,12 +4,23 @@ echo  start update server-backend
 echo =================================
 
 echo pull~~
-cd /usr/local/src/server-backend
+cd /usr/local/src/server-backend/deployment
 git pull
 echo pull compelete
 
 echo update
-docker stop server-backend
-docker rm server-backend
+name=server-backend 
+#判断是否容器存在
+docker ps -a | grep $name &> /dev/null
+#如果存在，关闭并删除该容器
+if [ $? -eq 0 ]
+then
+    echo $name" is up,we will stop and remove it!!!"
+    docker stop $name 
+    docker rm $name 
+else
+    echo $name" is not up!!!"
+fi
+cd /usr/local/src/server-backend/deployment
 docker-compose up -d --build
 echo update complete
